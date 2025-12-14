445d6251a1e8bc3c1177144eae5971a4979211bb
Jacob Choi <jchoi14@stevens.edu>
2025-12-14 14:51:04 -0500

    Fixed duplicate account creation and other related signup errors, added full functionality for movie creation, edit movie functionality, delete movie functionality, cleaned console logging

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,7 +3,6 @@
     if (inputForm) {
         inputForm.addEventListener('submit', (event) => {
             event.preventDefault()
-            console.log("in client side code")
             let username = document.getElementById("username").value
             let password = document.getElementById("password").value
             let cpassword = document.getElementById("cpassword").value
@@ -23,7 +22,7 @@
                 if (cpassword === "") throw "empty string"
 
                 if (password !== cpassword) throw "passwords don't match smh"
-                
+
                 if (!age) throw "missing age"
             } catch (e) {
                 event.preventDefault()
@@ -50,17 +49,15 @@
 
             $.ajax(requestConfig).then(function (responseMessage) {
                 console.log(responseMessage)
-                if(responseMessage.success){
-                    inputForm.replaceWith(`${responseMessage.message}`)
-                }else{
-                    console.log(responseMessage.message)
-                    //update error div with error message
-                    let p = document.createElement("p")
-                    p.textContent = `Error: ${responseMessage.message}`
-                    const errorDiv = document.getElementById("error")
-                    errorDiv.appendChild(p)
-                }
-                
+                inputForm.replaceWith(`${responseMessage.message}`)
+
+            }, function (errorMessage) {
+                console.log(errorMessage.message)
+                //update error div with error message
+                let p = document.createElement("p")
+                p.textContent = `Error in creating account. Username my already be taken.`
+                const errorDiv = document.getElementById("error")
+                errorDiv.replaceChildren(p)
             })
 
 
--- routes/accounts.js
+++ routes/accounts.js
@@ -61,7 +61,6 @@ router.route('/follow').post(async (req, res) => {
 
 router.route('/createaccount').get(async (req, res) => {
     try {
-        console.log("in create account")
         res.render('signup', { Title: "Signup" })
     } catch (e) {
         return res.status(500).render('error', {
@@ -117,7 +116,6 @@ router.route('/login').post(async (req, res) => {
 router.route('/signupconfirm').post(async (req, res) => {
     const accountsignupdata = req.body
     let account = {}
-    console.log("in signup confirm")
     let age = 0
     try {
         helpers.checkValidString(accountsignupdata.username)
@@ -127,8 +125,6 @@ router.route('/signupconfirm').post(async (req, res) => {
         helpers.checkValidString(accountsignupdata.password)
         accountsignupdata.password = xss(accountsignupdata.password.trim())
         helpers.checkValidString(accountsignupdata.password)
-
-        console.log(accountsignupdata)
 
         let age = Number(accountsignupdata.age)
         helpers.checkValidAge(age)
@@ -176,13 +172,11 @@ router.route('/myaccount').get(requireLogin, async (req, res) => {
         currentUserId = currentUserId.trim()
         helpers.checkValidId(currentUserId)
         curuser = await accountData.getAccountById(currentUserId)
-        console.log("current user: ")
-        console.log(curuser)
-        //statistics = await accountData.calculateStatistics(currentUserId)
-        //console.log(statistics)
         res.render('myaccount', {
             Title: "My Account",
-            account: curuser
+            username: curuser.username,
+            age: curuser.age,
+            profile_description: curuser.profile_description
         });
     } catch (e) {
         return res.status(401).render('error', {
@@ -314,7 +308,6 @@ router.route('/accountlookupresults').post(async (req, res) => {
     }
     try {
         let accounts = await accountData.getAccountByUsername(accountlookupdata.accountName)
-        console.log(accounts)
         res.render('accountlookupresults', { accounts: accounts, Title: accountlookupdata.accountName + " Results" })
     } catch (e) {
         return res.status(500).render('error', {
@@ -329,7 +322,6 @@ router.route('/:id').get(async (req, res) => {
     //createStatsObject(id)
     let account = {}
     let statistics = {}
-    console.log("in :id")
     try {
         helpers.checkValidId(id)
         id = id.trim()
